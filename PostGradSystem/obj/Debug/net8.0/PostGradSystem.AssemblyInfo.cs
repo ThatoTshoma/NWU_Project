f82d86a8a712589500da587f9d72bffa87dca722
Thato Tshoma <Thato.Tshoma@sita.co.za>
2026-05-19 05:05:53 +0200

    che

--- PostGradSystem/obj/Debug/net8.0/PostGradSystem.AssemblyInfo.cs
+++ PostGradSystem/obj/Debug/net8.0/PostGradSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PostGradSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4d73048706480dc2ac4e1bab1dcd7fe41b32b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f668048b4d71471f77a2f16cf60ed21f28fcd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PostGradSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PostGradSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
